a16638ac0b8aadb3312911734203c21f55842e08
Mr. Sanjoy <--add>
2023-12-27 12:54:31 +0530

    css working

---
+++ src/Componants/Sanjoy.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Sanjoy = () => {
+    return (
+        <>
+            <div className='self-center bg-green-200 flex justify-center'>
+                <img src='src\assets\images\sanjoy_photo.jpg ' className='w-28 rounded-lg border-4 border-pink-600 hover:opacity-70 ' />
+                <h1 className='text-green-600 italic font-bold'>Sanjoy Gorai</h1>
+                <p className='text-yellow-500'>I am Sanjoy from Asia. Country India.
+                    State of West Bengal, Distric of Bankura,
+                    from Chhatna,Pin: 722132 Village: Dubrajpur
+                </p>
+            </div>
+        </>
+    )
+}
+
+export default Sanjoy
